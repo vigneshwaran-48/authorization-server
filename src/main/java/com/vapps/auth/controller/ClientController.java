c9086d89ce94f80dd7fb6f585f84872a06fa8719
vigneshwaran48 <p3487260@gmail.com>
2024-07-17 21:55:31 +0530

    [#enhancement] Made client name unique across users and prefixing client name with the scopes.

--- src/main/java/com/vapps/auth/controller/ClientController.java
+++ src/main/java/com/vapps/auth/controller/ClientController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+import com.vapps.auth.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -17,12 +18,6 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.web.bind.annotation.*;
 
-import com.vapps.auth.dto.ClientControllerResponse;
-import com.vapps.auth.dto.ClientCreationPayload;
-import com.vapps.auth.dto.ClientDTO;
-import com.vapps.auth.dto.ClientResponseWithId;
-import com.vapps.auth.dto.EmptyResponse;
-import com.vapps.auth.dto.SingleClientControllerResponse;
 import com.vapps.auth.exception.AppException;
 import com.vapps.auth.service.ClientService;
 
@@ -32,115 +27,132 @@ import jakarta.servlet.http.HttpServletRequest;
 @RequestMapping("/api/user/{userId}/client")
 public class ClientController {
 
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-	@Autowired
-	private ClientService clientService;
+    @Autowired
+    private ClientService clientService;
 
-	private void getClientAuth(Set<ClientAuthenticationMethod> clientAuthenticationMethods) {
-		clientAuthenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_BASIC);
-		clientAuthenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_POST);
-		clientAuthenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_JWT);
-	}
+    private void getClientAuth(Set<ClientAuthenticationMethod> clientAuthenticationMethods) {
+        clientAuthenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_BASIC);
+        clientAuthenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_POST);
+        clientAuthenticationMethods.add(ClientAuthenticationMethod.CLIENT_SECRET_JWT);
+    }
 
-	private void getGrantTypes(Set<AuthorizationGrantType> authorizationGrantTypes) {
-		authorizationGrantTypes.add(AuthorizationGrantType.AUTHORIZATION_CODE);
-		authorizationGrantTypes.add(AuthorizationGrantType.CLIENT_CREDENTIALS);
-		authorizationGrantTypes.add(AuthorizationGrantType.DEVICE_CODE);
-		authorizationGrantTypes.add(AuthorizationGrantType.REFRESH_TOKEN);
-		authorizationGrantTypes.add(AuthorizationGrantType.JWT_BEARER);
-	}
+    private void getGrantTypes(Set<AuthorizationGrantType> authorizationGrantTypes) {
+        authorizationGrantTypes.add(AuthorizationGrantType.AUTHORIZATION_CODE);
+        authorizationGrantTypes.add(AuthorizationGrantType.CLIENT_CREDENTIALS);
+        authorizationGrantTypes.add(AuthorizationGrantType.DEVICE_CODE);
+        authorizationGrantTypes.add(AuthorizationGrantType.REFRESH_TOKEN);
+        authorizationGrantTypes.add(AuthorizationGrantType.JWT_BEARER);
+    }
 
-	@PostMapping
-	public ResponseEntity<?> addClient(@RequestBody ClientCreationPayload payload, Principal principal)
-			throws AppException {
+    @PostMapping
+    public ResponseEntity<?> addClient(@RequestBody ClientCreationPayload payload, Principal principal)
+            throws AppException {
 
-		if (principal == null || principal.getName() == null) {
-			throw new AppException(401, "User details not found");
-		}
-		List<String> redirectUris = Arrays.asList(payload.getRedirectUris().split(","));
-		List<String> scopes = Arrays.asList(payload.getScopes().split(","));
+        if (principal == null || principal.getName() == null) {
+            throw new AppException(401, "User details not found");
+        }
+        List<String> redirectUris = Arrays.asList(payload.getRedirectUris().split(","));
+        List<String> scopes = Arrays.asList(payload.getScopes().split(","));
 
-		String secret = payload.getClientSecret();
-		secret = passwordEncoder.encode(secret);
-		
-		RegisteredClient.Builder client = RegisteredClient.withId(UUID.randomUUID().toString()).clientSecret(secret)
-				.clientAuthenticationMethods(this::getClientAuth).clientName(payload.getClientName())
-				.scopes(scope -> scopes.forEach(scope::add))
-				.redirectUris(redirect -> redirectUris.forEach(redirect::add))
-				.authorizationGrantTypes(this::getGrantTypes);
+        String secret = payload.getClientSecret();
+        secret = passwordEncoder.encode(secret);
 
-		String clientId = clientService.addClient(principal.getName(), client);
+        RegisteredClient.Builder client = RegisteredClient.withId(UUID.randomUUID().toString()).clientSecret(secret)
+                .clientAuthenticationMethods(this::getClientAuth).clientName(payload.getClientName())
+                .scopes(scope -> scopes.forEach(scope::add))
+                .redirectUris(redirect -> redirectUris.forEach(redirect::add))
+                .authorizationGrantTypes(this::getGrantTypes);
 
-		ClientResponseWithId response = new ClientResponseWithId();
-		response.setClientId(clientId);
-		response.setMessage("success");
-		response.setStatus(HttpStatus.CREATED.value());
-		response.setPath("/api/user/" + principal.getName() + "/client");
-		response.setTimestamp(LocalDateTime.now());
+        String clientId = clientService.addClient(principal.getName(), client);
 
-		return ResponseEntity.ok(response);
-	}
+        ClientResponseWithId response = new ClientResponseWithId();
+        response.setClientId(clientId);
+        response.setMessage("success");
+        response.setStatus(HttpStatus.CREATED.value());
+        response.setPath("/api/user/" + principal.getName() + "/client");
+        response.setTimestamp(LocalDateTime.now());
 
-	@GetMapping
-	public ResponseEntity<?> getAllClient(@PathVariable("userId") String userId, Principal principal,
-			HttpServletRequest request) throws AppException {
+        return ResponseEntity.ok(response);
+    }
 
-		if (principal == null || principal.getName() == null || principal.getName().isEmpty()) {
-			throw new AppException(401, "Not Authenticated");
-		}
-		List<ClientDTO> clients = clientService.getAllClients(userId);
-		ClientControllerResponse clientResponse = new ClientControllerResponse(HttpStatus.OK.value(), "success",
-				LocalDateTime.now(), request.getServletPath(), clients);
+    @GetMapping
+    public ResponseEntity<?> getAllClient(@PathVariable("userId") String userId, Principal principal,
+            HttpServletRequest request) throws AppException {
 
-		return ResponseEntity.ok(clientResponse);
-	}
+        if (principal == null || principal.getName() == null || principal.getName().isEmpty()) {
+            throw new AppException(401, "Not Authenticated");
+        }
+        List<ClientDTO> clients = clientService.getAllClients(userId);
+        ClientControllerResponse clientResponse =
+                new ClientControllerResponse(HttpStatus.OK.value(), "success", LocalDateTime.now(),
+                        request.getServletPath(), clients);
 
-	@GetMapping("{clientId}")
-	public ResponseEntity<?> getClient(@PathVariable("userId") String userId, @PathVariable("clientId") String clientId,
-			Principal principal, HttpServletRequest request) throws AppException {
+        return ResponseEntity.ok(clientResponse);
+    }
 
-		if (principal == null || principal.getName() == null || principal.getName().isEmpty()) {
-			throw new AppException(401, "Not Authenticated");
-		}
-		ClientDTO client = clientService.getClientById(userId, clientId);
-		SingleClientControllerResponse clientResponse = new SingleClientControllerResponse(HttpStatus.OK.value(),
-				"success", LocalDateTime.now(), request.getServletPath(), client);
+    @GetMapping("{clientId}")
+    public ResponseEntity<?> getClient(@PathVariable("userId") String userId, @PathVariable("clientId") String clientId,
+            Principal principal, HttpServletRequest request) throws AppException {
 
-		return ResponseEntity.ok(clientResponse);
-	}
+        if (principal == null || principal.getName() == null || principal.getName().isEmpty()) {
+            throw new AppException(401, "Not Authenticated");
+        }
+        ClientDTO client = clientService.getClientById(userId, clientId);
+        SingleClientControllerResponse clientResponse =
+                new SingleClientControllerResponse(HttpStatus.OK.value(), "success", LocalDateTime.now(),
+                        request.getServletPath(), client);
 
-	@DeleteMapping("{clientId}")
-	public ResponseEntity<?> deleteClient(@PathVariable("userId") String userId,
-			@PathVariable("clientId") String clientId, Principal principal) throws AppException {
-		if (principal == null || principal.getName() == null || principal.getName().isEmpty()) {
-			throw new AppException(401, "Not Authenticated");
-		}
-		clientService.removeClient(userId, clientId);
+        return ResponseEntity.ok(clientResponse);
+    }
 
-		EmptyResponse response = new EmptyResponse();
-		response.setMessage("Successfully deleted client");
-		response.setPath("/api/user/" + userId + "/client/" + clientId);
-		response.setStatus(HttpStatus.OK.value());
-		response.setTimestamp(LocalDateTime.now());
+    @DeleteMapping("{clientId}")
+    public ResponseEntity<?> deleteClient(@PathVariable("userId") String userId,
+            @PathVariable("clientId") String clientId, Principal principal) throws AppException {
+        if (principal == null || principal.getName() == null || principal.getName().isEmpty()) {
+            throw new AppException(401, "Not Authenticated");
+        }
+        clientService.removeClient(userId, clientId);
 
-		return ResponseEntity.ok(response);
-	}
+        EmptyResponse response = new EmptyResponse();
+        response.setMessage("Successfully deleted client");
+        response.setPath("/api/user/" + userId + "/client/" + clientId);
+        response.setStatus(HttpStatus.OK.value());
+        response.setTimestamp(LocalDateTime.now());
 
-	@PutMapping("{clientId}")
-	public ResponseEntity<?> updateClient(@PathVariable("userId") String userId,
-			@PathVariable("clientId") String clientId, @RequestBody ClientDTO clientDetails,
-			Principal principal) throws AppException {
-		clientDetails.setClientId(clientId);
-		clientService.updateClient(userId, clientDetails);
+        return ResponseEntity.ok(response);
+    }
 
-		EmptyResponse response = new EmptyResponse();
-		response.setMessage("Updated client successfully");
-		response.setTimestamp(LocalDateTime.now());
-		response.setPath("/api/user/" + userId + "/client/" + clientId);
-		response.setStatus(HttpStatus.OK.value());
+    @PutMapping("{clientId}")
+    public ResponseEntity<?> updateClient(@PathVariable("userId") String userId,
+            @PathVariable("clientId") String clientId, @RequestBody ClientDTO clientDetails, Principal principal)
+            throws AppException {
+        clientDetails.setClientId(clientId);
+        clientService.updateClient(userId, clientDetails);
 
-		return ResponseEntity.ok(response);
-	}
+        EmptyResponse response = new EmptyResponse();
+        response.setMessage("Updated client successfully");
+        response.setTimestamp(LocalDateTime.now());
+        response.setPath("/api/user/" + userId + "/client/" + clientId);
+        response.setStatus(HttpStatus.OK.value());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("{clientName}/exists")
+    public ResponseEntity<?> isClientExistsWithName(@PathVariable("userId") String userId,
+            @PathVariable String clientName, Principal principal) throws AppException {
+        boolean isExists = clientService.isClientExistsByName(clientName);
+
+        ClientExistsResponse response = new ClientExistsResponse();
+        response.setMessage("Is client exists: " + isExists);
+        response.setTimestamp(LocalDateTime.now());
+        response.setPath("/api/user/" + userId + "/client/" + clientName + "/exists");
+        response.setStatus(HttpStatus.OK.value());
+		response.setClientExists(isExists);
+
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/vapps/auth/service/ClientServiceImpl.java
+++ src/main/java/com/vapps/auth/service/ClientServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -30,193 +31,214 @@ import com.vapps.auth.repository.ClientRepository;
 public class ClientServiceImpl implements ClientService {
 
     @Autowired
-	private ClientRepository clientRepository;
+    private ClientRepository clientRepository;
 
     @Autowired
     private ScopeService scopeService;
 
-	@Autowired
-	private AppUserService appUserService;
+    @Autowired
+    private AppUserService appUserService;
 
-	@Autowired
-	private TokenSettings tokenSettings;
+    @Autowired
+    private TokenSettings tokenSettings;
 
-	@Autowired
-	private ClientSettings clientSettings;
+    @Autowired
+    private ClientSettings clientSettings;
 
-	@Value("${app.default.scopes}")
-	private String defaultScopes;
+    @Value("${app.default.scopes}")
+    private String defaultScopes;
 
-	@Value("${app.client.clientIdPrefix}")
-	private String clientIdPrefix;
+    @Value("${app.client.clientIdPrefix}")
+    private String clientIdPrefix;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ClientServiceImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientServiceImpl.class);
+    private static final Pattern CLIENT_NAME_REGEX = Pattern.compile("^[a-zA-Z0-9.]+$");
 
     @Override
     public String addClient(ClientDTO client) throws AppException {
         Assert.notNull(client, "Client can't be null");
-		Client addedClient = clientRepository.save(Client.build(client));
-		if(addedClient != null) {
-			return addedClient.getId();
-		}
-		throw new AppException("Can't create the client");
+        Client addedClient = clientRepository.save(Client.build(client));
+        if (addedClient != null) {
+            return addedClient.getId();
+        }
+        throw new AppException("Can't create the client");
     }
 
     @Override
     public String addClient(String userId, Builder registeredClient) throws AppException {
         UserDTO appUser = appUserService.findByUserId(userId);
-		if(appUser == null) {
-			throw new AppException("User not found for " + userId);
-		}
-		String clientId = UUID.randomUUID().toString();
-		registeredClient
-				.clientId(clientIdPrefix + "-" + clientId.toString())
-				.tokenSettings(tokenSettings)
-				.clientSettings(clientSettings);
+        if (appUser == null) {
+            throw new AppException("User not found for " + userId);
+        }
+        String clientId = UUID.randomUUID().toString();
+        registeredClient.clientId(clientIdPrefix + "-" + clientId.toString()).tokenSettings(tokenSettings)
+                .clientSettings(clientSettings);
 
-		Client client = Client.build(registeredClient.build());
+        Client client = Client.build(registeredClient.build());
 
-		if(clientRepository.findByUserIdAndClientName(userId, client.getClientName()).isPresent()) {
-			throw new AppException(HttpStatus.BAD_REQUEST.value(), "Client already existing with this name");
-		}
-		client.setUser(AppUser.build(appUser));
-		Client addedClient = clientRepository.save(client);
+        if (client.getClientName() == null || !CLIENT_NAME_REGEX.matcher(client.getClientName()).matches()) {
+            throw new AppException(HttpStatus.BAD_REQUEST.value(), "Invalid client name!");
+        }
 
-		if(addedClient != null) {
-			if(!isDefaultScopes(addedClient.getScopes())) {
-				LOGGER.info("Adding scopes to db ...");
-				addScopes(addedClient, addedClient.getScopes());
-			}
-			return addedClient.getClientId();
-		}
-		throw new AppException("Can't create the client");
+        if (clientRepository.findByClientNameIgnoreCase(client.getClientName()).isPresent()) {
+            throw new AppException(HttpStatus.BAD_REQUEST.value(), "Client already existing with this name");
+        }
+        client.setUser(AppUser.build(appUser));
+        formatScopes(client);
+        Client addedClient = clientRepository.save(client);
+
+        if (addedClient != null) {
+            if (!isDefaultScopes(addedClient.getScopes())) {
+                LOGGER.info("Adding scopes to db ...");
+                addScopes(addedClient, addedClient.getScopes());
+            }
+            return addedClient.getClientId();
+        }
+        throw new AppException("Can't create the client");
     }
 
     @Override
     public boolean isClientExists(String clientId) {
         Assert.notNull(clientId, "Client id can't be null");
-		return clientRepository.findByClientId(clientId).isPresent();
+        return clientRepository.findByClientId(clientId).isPresent();
+    }
+
+    @Override
+    public boolean isClientExistsByName(String clientName) {
+        return clientRepository.findByClientNameIgnoreCase(clientName).isPresent();
     }
 
     @Override
     public void removeClient(String userId, String clientId) {
         Assert.notNull(clientId, "Client id can't be null");
-		scopeService.deleteAllScopesOfClient(userId, clientId);
-		clientRepository.deleteByClientId(clientId);
+        scopeService.deleteAllScopesOfClient(userId, clientId);
+        clientRepository.deleteByClientId(clientId);
     }
 
     @Override
     public List<ClientDTO> getAllClients(String userId) {
         Preconditions.checkArgument(userId != null, "User id required");
-		
-		List<Client> clients = clientRepository.findByUserId(userId);
-		return clients.stream()
-					  .map(client -> client.toDTO())
-					  .collect(Collectors.toList());
+
+        List<Client> clients = clientRepository.findByUserId(userId);
+        return clients.stream().map(client -> client.toDTO()).collect(Collectors.toList());
     }
 
     @Override
     public ClientDTO getClientById(String userId, String clientId) {
         Client client = clientRepository.findByClientIdAndUserId(clientId, userId)
-					.orElseThrow(() -> new IllegalArgumentException("Client id not exists"));
-		
-		return client.toDTO();
+                .orElseThrow(() -> new IllegalArgumentException("Client id not exists"));
+
+        return client.toDTO();
     }
 
     @Override
     public void updateClient(String userId, ClientDTO clientDetails) throws AppException {
-        Preconditions.checkArgument(clientDetails != null,
-									"Client data can't be empty");
-		Preconditions.checkArgument(userId != null && !userId.isBlank(),
-				"User id can't be empty");
+        Preconditions.checkArgument(clientDetails != null, "Client data can't be empty");
+        Preconditions.checkArgument(userId != null && !userId.isBlank(), "User id can't be empty");
 
-		Optional<Client> prevClient =
-					clientRepository
-							.findByClientIdAndUserId(
-									clientDetails.getClientId(),
-									userId);
-		if(prevClient.isEmpty()) {
-			throw new AppException(HttpStatus.BAD_REQUEST.value(), "Client not exists");
-		}
-		if(clientDetails.getClientName() != null) {
-			Optional<Client> temp = clientRepository
-					.findByUserIdAndClientName(userId, clientDetails.getClientName());
-			if(temp.isPresent() && !temp.get().getClientName().equals(clientDetails.getClientName())) {
-				throw new AppException(HttpStatus.BAD_REQUEST.value(), "Client already existing with this name");
-			}
-		}
+        Optional<Client> prevClient = clientRepository.findByClientIdAndUserId(clientDetails.getClientId(), userId);
+        if (prevClient.isEmpty()) {
+            throw new AppException(HttpStatus.BAD_REQUEST.value(), "Client not exists");
+        }
+        if (clientDetails.getClientName() != null) {
+            Optional<Client> temp = clientRepository.findByClientNameIgnoreCase(clientDetails.getClientName());
+            if (temp.isPresent() && !temp.get().getClientName().equals(clientDetails.getClientName())) {
+                throw new AppException(HttpStatus.BAD_REQUEST.value(), "Client already existing with this name");
+            }
+        }
 
-		clientDetails.setUserDTO(prevClient.get().getUser().toDTO());
-		Client newClient = Client.build(clientDetails);
-		checkAndUpdateClient(prevClient.get(), newClient);
+        clientDetails.setUserDTO(prevClient.get().getUser().toDTO());
+        Client newClient = Client.build(clientDetails);
+        formatScopes(newClient);
+        checkAndUpdateClient(prevClient.get(), newClient);
 
-		//TODO Need to validate the client details before updating
-		clientRepository.save(newClient);
+        //TODO Need to validate the client details before updating
+        clientRepository.save(newClient);
     }
 
-	private boolean isDefaultScopes(String clientScopes) {
-		List<String> clientScopesList = List.of(clientScopes.split(","));
-		List<String> defaultScopesList = List.of(defaultScopes.split(","));
+    private boolean isDefaultScopes(String clientScopes) {
+        List<String> clientScopesList = List.of(clientScopes.split(","));
+        List<String> defaultScopesList = List.of(defaultScopes.split(","));
 
-		for(String scope : clientScopesList) {
-			if(!defaultScopesList.contains(scope)) {
-				return false;
-			}
-		}
-		return true;
-	}
+        for (String scope : clientScopesList) {
+            if (!defaultScopesList.contains(scope)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	private void addScopes(Client client, String scopes) throws AppException {
-		List<ScopeDTO> scopeDetails = Arrays
-				.stream(scopes.split(","))
-				.map(scope -> {
-					ScopeDTO scopeDetail = new ScopeDTO();
-					scopeDetail.setClient(client.toDTO());
-					scopeDetail.setScopeName(scope);
-					return scopeDetail;
-				})
-				.toList();
-		scopeService.checkAndScopes(client.toDTO(), scopeDetails);
-	}
+    private void addScopes(Client client, String scopes) throws AppException {
+        List<ScopeDTO> scopeDetails = Arrays.stream(scopes.split(",")).map((final String scope) -> {
+            ScopeDTO scopeDetail = new ScopeDTO();
+            /**
+             * Adding scopes with client name prefix if it is not a default scope like openid
+             */
+            scopeDetail.setScopeName(scope);
+            if (!Arrays.stream(defaultScopes.split(","))
+                    .anyMatch(defaultScope -> defaultScope.equalsIgnoreCase(scope))) {
+                scopeDetail.setScopeName(client.getClientName() + "." + scope);
+            }
+            scopeDetail.setClient(client.toDTO());
+            return scopeDetail;
+        }).toList();
+        scopeService.checkAndScopes(client.toDTO(), scopeDetails);
+    }
+
+    private void formatScopes(Client client) {
+        StringBuffer scopes = new StringBuffer();
+        for (String scope : client.getScopes().split(",")) {
+            if (!Arrays.stream(defaultScopes.split(","))
+                    .anyMatch(defaultScope -> defaultScope.equalsIgnoreCase(scope)) && !scope.toLowerCase()
+                    .startsWith((client.getClientName() + ".").toLowerCase())) {
+                scopes.append(client.getClientName() + "." + scope).append(",");
+                continue;
+            }
+            scopes.append(scope).append(",");
+        }
+        if (scopes.length() > 0) {
+            scopes.deleteCharAt(scopes.length() - 1);
+        }
+        client.setScopes(scopes.toString());
+    }
 
     private void checkAndUpdateClient(Client prevClient, Client newClient) {
-		if(newClient.getId() == null) {
-			newClient.setId(prevClient.getId());
-		}
-		if(newClient.getClientSecret() == null) {
-			newClient.setClientSecret(prevClient.getClientSecret());
-		}
-		if(newClient.getClientIdIssuedAt() == null) {
-			newClient.setClientIdIssuedAt(prevClient.getClientIdIssuedAt());
-		}
-		if(newClient.getClientName() == null) {
-			newClient.setClientName(prevClient.getClientName());
-		}
-		if(newClient.getClientSettings() == null) {
-			newClient.setClientSettings(prevClient.getClientSettings());
-		}
-		if(newClient.getAuthorizationGrantTypes() == null) {
-			newClient.setAuthorizationGrantTypes(prevClient.getAuthorizationGrantTypes());
-		}
-		if(newClient.getClientAuthenticationMethods() == null) {
-			newClient.setClientAuthenticationMethods(
-					prevClient.getClientAuthenticationMethods());
-		}
-		if(newClient.getClientSecretExpiresAt() == null) {
-			newClient.setClientSecretExpiresAt(prevClient.getClientSecretExpiresAt());
-		}
-		if(newClient.getRedirectUris() == null) {
-			newClient.setRedirectUris(prevClient.getRedirectUris());
-		}
-		if(newClient.getScopes() == null) {
-			newClient.setScopes(prevClient.getScopes());
-		}
-		if(newClient.getTokenSettings() == null) {
-			newClient.setTokenSettings(prevClient.getTokenSettings());
-		}
-		if(newClient.getUser() == null) {
-			newClient.setUser(prevClient.getUser());
-		}
-	}
-    
+        if (newClient.getId() == null) {
+            newClient.setId(prevClient.getId());
+        }
+        if (newClient.getClientSecret() == null) {
+            newClient.setClientSecret(prevClient.getClientSecret());
+        }
+        if (newClient.getClientIdIssuedAt() == null) {
+            newClient.setClientIdIssuedAt(prevClient.getClientIdIssuedAt());
+        }
+        if (newClient.getClientName() == null) {
+            newClient.setClientName(prevClient.getClientName());
+        }
+        if (newClient.getClientSettings() == null) {
+            newClient.setClientSettings(prevClient.getClientSettings());
+        }
+        if (newClient.getAuthorizationGrantTypes() == null) {
+            newClient.setAuthorizationGrantTypes(prevClient.getAuthorizationGrantTypes());
+        }
+        if (newClient.getClientAuthenticationMethods() == null) {
+            newClient.setClientAuthenticationMethods(prevClient.getClientAuthenticationMethods());
+        }
+        if (newClient.getClientSecretExpiresAt() == null) {
+            newClient.setClientSecretExpiresAt(prevClient.getClientSecretExpiresAt());
+        }
+        if (newClient.getRedirectUris() == null) {
+            newClient.setRedirectUris(prevClient.getRedirectUris());
+        }
+        if (newClient.getScopes() == null) {
+            newClient.setScopes(prevClient.getScopes());
+        }
+        if (newClient.getTokenSettings() == null) {
+            newClient.setTokenSettings(prevClient.getTokenSettings());
+        }
+        if (newClient.getUser() == null) {
+            newClient.setUser(prevClient.getUser());
+        }
+    }
+
 }
